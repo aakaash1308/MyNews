0dce883663c9f14b338a515b6de105742881e5ea
aakaashkapoor <aakaash1308@gmail.com>
2017-10-12 15:34:52 -0700

    push this

--- app/src/main/java/com/example/aakaashkapoor/mynews/getJsonData.java
+++ app/src/main/java/com/example/aakaashkapoor/mynews/getJsonData.java
@@ -25,11 +25,19 @@ public class getJsonData extends AsyncTask<Void,Void,Void> {
     String jsonData = "";
     JSONObject jsonObject;
 
+    String URLs [] = {
+            "https://api.newsapi.aylien.com/api/v1/stories?categories.taxonomy=iptc-subjectcode&categories.confident=true&categories.id%5B%5D=11000000&source.name%5B%5D=BBC&cluster=false&cluster.algorithm=lingo&sort_by=published_at&sort_direction=desc&cursor=*&per_page=10"
+            ,"a"
+    };
+
     @Override
     protected Void doInBackground(Void... voids) {
         try {
-            URL url = new URL("https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=eaef4430cc804415b2d2cba50782913b");
+            Log.i("I am currently here", " yes");
+            URL url = new URL(URLs[0]);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.addRequestProperty("X-AYLIEN-NewsAPI-Application-ID","15c136e0" );
+            httpURLConnection.addRequestProperty("X-AYLIEN-NewsAPI-Application-Key", "15c7da7bb8d73f21f0af9bf5ef6d2539");
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             StringBuilder responseStrBuilder = new StringBuilder();
@@ -38,6 +46,7 @@ public class getJsonData extends AsyncTask<Void,Void,Void> {
                 responseStrBuilder.append(inputStr);
 
             jsonObject = new JSONObject(responseStrBuilder.toString());
+            //Log.i("news", jsonObject.toString());
 
 //            String line = "";
 //
@@ -59,36 +68,37 @@ public class getJsonData extends AsyncTask<Void,Void,Void> {
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        
-        MainActivity.title.setText(jsonData);
-        //MainActivity.articleNames.add();
-        
-        JSONArray jsonArray = null;
-        try {
-            jsonArray = jsonObject.getJSONArray("articles");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
-
-        int i = 0;
-        while(i < jsonArray.length())
-        {
-            try {
-                JSONObject array = new JSONObject(jsonArray.get(i).toString());
-                //Log.i("json", String.valueOf(array));
-                //MainActivity.title.append(array.get("title").toString());
-                //MainActivity.title.append("\n\n");
-
-
-                ArticleChoiceActivity.articleImages.add(array.get("urlToImage").toString());
-                ArticleChoiceActivity.articleNames.add(array.get("title").toString());
-                ArticleChoiceActivity.articleURL.add(array.get("url").toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            i++;
-        }
+        Log.i("news", jsonData);
+//        MainActivity.title.setText(jsonData);
+//        //MainActivity.articleNames.add();
+//
+//        JSONArray jsonArray = null;
+//        try {
+//            jsonArray = jsonObject.getJSONArray("articles");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        int i = 0;
+//        while(i < jsonArray.length())
+//        {
+//            try {
+//                JSONObject array = new JSONObject(jsonArray.get(i).toString());
+//                //Log.i("json", String.valueOf(array));
+//                //MainActivity.title.append(array.get("title").toString());
+//                //MainActivity.title.append("\n\n");
+//
+//
+//                ArticleChoiceActivity.articleImages.add(array.get("urlToImage").toString());
+//                ArticleChoiceActivity.articleNames.add(array.get("title").toString());
+//                ArticleChoiceActivity.articleURL.add(array.get("url").toString());
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//
+//            i++;
+//        }
     }
 }
